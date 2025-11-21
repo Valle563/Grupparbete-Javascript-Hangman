a50d80572ccf1af8c105818dadf77c15be03e26e
Antonina Dubrovska <antonina.dubrovska@gmail.com>
2025-11-21 14:21:53 +0100

    Add visual feedback for keyboard guesses
     * Made keyboard interactive (highlight correct guesses in orange and wrong guesses in light gray)
    *The createKeyboard function now accepts an onGuess callback, and handleGuess updates button styles and disables them after selection.
    * Created a temporary secretWord

--- src/word-management.js
+++ src/word-management.js
@@ -1,11 +1,11 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
 
-function createKeyboard(container) {
+function createKeyboard(container, onGuess) {
 	container.innerHTML = ''
 	for (const letter of letters) {
 		const keyButton = document.createElement("button")
 		keyButton.innerText = letter
-		keyButton.classList.add("keyButton") 
+		keyButton.classList.add("keyButton")
 
 		keyButton.addEventListener("click", () => {
 		  onGuess(letter, keyButton)
@@ -14,12 +14,22 @@ function createKeyboard(container) {
 		container.appendChild(keyButton)
 	  }
 }
-
-
+// TODO export/import to a 'main/gamelogic.js' (to be created):
 const keyboardContainer = document.querySelector(".keyboard")
 let secretWord = "KARDEMUMMA" // example ord
 
 let correct = 0
 let wrong = 0
 
-createKeyboard(keyboardContainer)
+createKeyboard(keyboardContainer, handleGuess)
+
+ function handleGuess(letter, button) {
+  if (secretWord.includes(letter)) {
+    button.classList.add("correct")
+    correct++
+  } else {
+    button.classList.add("wrong")
+    wrong++
+  }
+  button.disabled = true
+}
