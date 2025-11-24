a1917082f8b035f7187655e36c826754c1d78dc3
Valentino Finocchiaro <Valentino.finocchiaro@student.nbi-handelsakademin.se>
2025-11-24 10:10:05 +0100

    added head and body to drawman

--- src/drawhangman.js
+++ src/drawhangman.js
@@ -1,14 +1,25 @@
-const parts = document.querySelectorAll('.part')
-let step = 0
+const parts = [ 
+  document.querySelector("#head"),
+  document.querySelector("#body"),
+  document.querySelector("#arm-left"),
+  document.querySelector("#arm-right"),
+  document.querySelector("#leg-left"),
+  document.querySelector("#leg-right")
+]
 
-document.getElementById('nextPart').addEventListener('click', () =>{
-    if (step < parts.length){
-        parts[step].computedStyleMap.display = 'block';
+let step = 0;
+
+function showNextPart(){
+     if (step < parts.length) {
+        parts[step].style.display = 'block';
         step++;
     }
-})
+}
 
-document.getElementById('resetMan').addEventListener('click', () => {
-    parts.forEach(part => part.computedStyleMap.display = 'none')
-    step = 0
-})
+function resetMan () {
+    parts.forEach(part => part.style.display = 'none')
+    step = 0;
+}
+
+document.querySelector("#next").addEventListener("click", showNextPart);
+document.querySelector("#reset").addEventListener("click", resetMan);
